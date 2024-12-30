140da7d18a86662a0ae7ced5d65767f2e85c1a48
Magrosunrg <magrosunrg@gmail.com>
2024-12-30 13:01:21 +0200

    model2

---
+++ model2/model2.js
@@ -0,0 +1,37 @@
+const images = Array.from({ length: 400 }, (_, i) => `${String(i + 1).padStart(4, '0')}.png`);
+
+let currentIndex = 0;
+let prevMouseX = null;
+
+const imageContainer = document.querySelector('.image-container');
+
+const preloadedImages = [];
+images.forEach((image) => {
+  const img = new Image();
+  img.src = image;
+  preloadedImages.push(img);
+});
+
+function changeBackground(reverse = false) {
+  if (!reverse) 
+  {
+    currentIndex = Math.min(currentIndex + 1, images.length - 1);
+  } else 
+  {
+    currentIndex = Math.max(currentIndex - 1, 0);
+  }
+
+  imageContainer.style.backgroundImage = `url('${images[currentIndex]}')`;
+}
+
+imageContainer.addEventListener('mousemove', (event) => {
+  if (prevMouseX !== null) {
+    if (event.clientX > prevMouseX) {
+      changeBackground(false);
+    } else if (event.clientX < prevMouseX) {
+      changeBackground(true);
+    }
+  }
+
+  prevMouseX = event.clientX;
+});
